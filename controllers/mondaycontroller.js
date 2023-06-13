5189375a8aee548d9e441b1171c33cf8b06f3f2f
PlusOneXD9 <rgharia@plusonex.com>
2023-06-13 17:39:40 +0530

    change controller

--- controllers/mondaycontroller.js
+++ controllers/mondaycontroller.js
@@ -1,19 +1,19 @@
 module.exports = async (req, res) => {
-  {
-    let query = "{ boards (limit:5) {name id} }";
-
-    let result = await fetch("https://api.monday.com/v2", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzc1NDI3OCwidWlkIjoyNzY5ODA5MSwiaWFkIjoiMjAyMy0wMi0xNlQxMjoxNTowNi4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTk2NjQyOSwicmduIjoidXNlMSJ9.7MM07gU0ivrKbXsNFQzhp8aYlgrWt4v2oNtNYTIiOHI",
-      },
-      body: JSON.stringify({
-        query: query,
-      }),
-    });
-    result = await result.json();
-    res.json(result);
-  }
-};
+    {
+      let query = "{ boards (limit:5) {name id} }";
+  
+      let result = await fetch("https://api.monday.com/v2", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzc1NDI3OCwidWlkIjoyNzY5ODA5MSwiaWFkIjoiMjAyMy0wMi0xNlQxMjoxNTowNi4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTk2NjQyOSwicmduIjoidXNlMSJ9.7MM07gU0ivrKbXsNFQzhp8aYlgrWt4v2oNtNYTIiOHI",
+        },
+        body: JSON.stringify({
+          query: query,
+        }),
+      });
+      result = await result.json();
+      res.json(result);
+    }
+  };
