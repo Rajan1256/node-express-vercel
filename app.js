bc45b9dac3038a3d95eadf50ac61846300f6860e
PlusOneXD9 <rgharia@plusonex.com>
2023-06-13 15:44:24 +0530

    change route

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const monday = require('./routers/mondayrouter');
 
 const app = express();
 app.use(express.json());
-app.use(monday);
 app.use(directus);
+app.use(monday);
 
 app.listen(4000, () => {
   console.log("listening on http://localhost:4000");
--- routers/directusrouter.js
+++ routers/directusrouter.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const directuscnt = require('../controllers/directuscontroller')
+const mondaycnt = require('../controllers/mondaycontroller')
 
 const app = Router();
 app.get('/', directuscnt);
+app.get('/monday', mondaycnt);
 module.exports = app;
